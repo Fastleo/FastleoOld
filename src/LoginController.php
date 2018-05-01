5f5d4508a5de398713eb50059e22f4764edb4616
Roman Zakharov <camanru@ya.ru>
2018-05-01 14:57:59 +0300

    use AuthenticatesUsers

--- src/LoginController.php
+++ src/LoginController.php
@@ -4,9 +4,33 @@ namespace Camanru\Fastleo;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Authorization
+     * @return mixed
+     */
     public function login()
     {
         return view('fastleo::login');
