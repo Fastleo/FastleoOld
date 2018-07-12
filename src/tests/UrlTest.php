63247ecaacacee25e8b5822ce328db7b64ef2670
Roman Zakharov <camanru@ya.ru>
2018-07-12 12:56:36 +0300

    tests

---
+++ src/tests/UrlTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Camanru\Fastleo;
+
+use Tests\TestCase;
+
+class UrlTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testMain()
+    {
+        $response = $this->get('/fastleo');
+        $response->assertStatus(500);
+    }
+}
