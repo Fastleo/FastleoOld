3644f468d8f7ad7d8ccf0029b53832652eeff280
Roman Zakharov <camanru@gmail.com>
2018-08-21 12:15:41 +0300

    fix log view

--- src/resources/views/log.blade.php
+++ src/resources/views/log.blade.php
@@ -3,12 +3,15 @@
 @section('content')
 
     <div class="row">
+        <div class="col">
+            <h3>Laravel error log</h3>
+        </div>
         <div class="col text-right">
             <a href="/fastleo/log/clear" class="btn btn-warning">Очистить log</a>
         </div>
     </div>
     <div class="row">
-        <div class="col">
+        <div class="col" style="max-height: 80vh; overflow-y: scroll;  overflow-x: hidden;">
             @foreach($logs as $log)
                 <small>{{ $log }}</small><br><br>
             @endforeach
